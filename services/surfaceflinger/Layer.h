21e645ff8f25649320d69e4d0aa12664514f5810
tandob-mtk <meetsoumen.das1@gmail.com>
2016-08-23 16:27:22 +0530

    Revert libgui: Hook up onFrameReplaced

--- services/surfaceflinger/Layer.h
+++ services/surfaceflinger/Layer.h
@@ -375,9 +375,9 @@ protected:
 
 private:
     // Interface implementation for SurfaceFlingerConsumer::ContentsChangedListener
-    virtual void onFrameAvailable(const BufferItem& item) override;
-    virtual void onFrameReplaced(const BufferItem& item) override;
-    virtual void onSidebandStreamChanged() override;
+    virtual void onFrameAvailable(const BufferItem& item);
+    virtual void onFrameReplaced(const BufferItem& item);
+    virtual void onSidebandStreamChanged();
 
     void commitTransaction();
 
@@ -463,3 +463,4 @@ private:
 }; // namespace android
 
 #endif // ANDROID_LAYER_H
+
